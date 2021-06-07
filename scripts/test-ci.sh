6870ff73ac467fd97cc1c536b6e6cf10d7a2eb61
Guy Sherman <guy@guysherman.com>
2021-06-07 20:33:26 +1200

    Tweaked test scripts so that tests run in docker
    We'll need a database all the time, and running locally
    would have a different hostname for the db, so better
    to just use the ci option

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -1,4 +1,3 @@
-
 #! /bin/bash
 
 set -euo pipefail
@@ -7,6 +6,8 @@ docker-compose -f docker-compose.testrunner.yml up -d db
 trap "docker-compose -f docker-compose.testrunner.yml down" EXIT
 echo "--- :docker: Build Test runner image"
 docker-compose -f docker-compose.testrunner.yml build test-runner
-# echo "+++ :docker: Testing for code smells"
+echo "+++ :docker: Testing for code smells"
 docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn lint
 docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn typecheck
+echo "+++ :docker: Running unit tests"
+docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn test:all
