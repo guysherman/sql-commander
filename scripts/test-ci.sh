4d7b29377da4ec650267d6f7f3455ebe8412a462
Guy Sherman <g.sherman@crimsoneducation.org>
2021-06-15 20:12:35 +1200

    Tidied up some configs

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -10,4 +10,4 @@ echo "+++ :docker: Testing for code smells"
 docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn lint
 docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn typecheck
 echo "+++ :docker: Running unit tests"
-docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn test:all
+docker-compose -f docker-compose.testrunner.yml run --rm test-runner yarn jest --testPathIgnorePatterns=dist/
