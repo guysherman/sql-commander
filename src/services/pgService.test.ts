bfa57e6c7ce998b37d499bb4bd00a0903896c8cf
Guy Sherman <g.sherman@crimsoneducation.org>
2021-06-17 22:20:02 +1200

    Removed an errant console.log

--- src/services/pgService.test.ts
+++ src/services/pgService.test.ts
@@ -8,7 +8,6 @@ beforeEach(async () => {
 
 test('returns a client that can query query the database', async () => {
   const profile = getProfile('real')
-  console.log(`Profile:\n\n${JSON.stringify(profile, null, '  ')}`)
   const conn = await getDbConnection(profile)
 
   const result:QueryResult<any> = await conn.query('SELECT 1 AS col')
