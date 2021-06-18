db75dcacde78e51df423bd56df521cb1b70845d4
Guy Sherman <g.sherman@crimsoneducation.org>
2021-06-18 23:53:26 +1200

    Finished listDatabases, listSchemas

--- src/services/metadataService.test.ts
+++ src/services/metadataService.test.ts
@@ -1,7 +1,7 @@
 import { PoolClient } from 'pg'
 import { loadProfiles, getProfile, Profile } from '../configuration'
 import { getDbConnection } from './pgService'
-import { listDatabases } from './metadataService'
+import { Database, listDatabases, listSchemas, Schema } from './metadataService'
 
 let profile: Profile
 let conn: PoolClient
@@ -9,7 +9,6 @@ let conn: PoolClient
 beforeEach(async () => {
   await loadProfiles('/opt/app/data/tests/real-test-profiles.json')
   profile = getProfile('real')
-  console.log(JSON.stringify(profile, null, '  '))
   conn = await getDbConnection(profile)
 })
 
@@ -17,9 +16,22 @@ afterEach(() => {
   conn.release()
 })
 
-test('listDatabases() returns correct list of databases from valid connection', async () => {
-  const results: string[] = await listDatabases(conn)
-  expect(results.length).toBe(4)
-  expect(false).toBe(true)
+test('listDatabases() doesn\'t included template* databases', async () => {
+  const results: Database[] = await listDatabases(conn)
+  expect(results.length).toBe(2)
+  const resultNames = results.map(r => r.name)
+  expect(resultNames).toContain('postgres')
+  expect(resultNames).toContain('sqlcmdr')
 })
 
+test('listSchemas() doesn\'t include pg_ schemas', async () => {
+  const results: Schema[] = await listSchemas(conn)
+  expect(results.length).toBe(2)
+
+  const noPg = results
+    .map(r => r.name)
+    .reduce((accum: boolean, curr: string) =>
+      accum && !curr.startsWith('pg_'), true)
+
+  expect(noPg).toBe(true)
+})
--- src/services/metadataService.ts
+++ src/services/metadataService.ts
@@ -1,7 +1,46 @@
-import { PoolClient } from 'pg'
+import { PoolClient, QueryResult } from 'pg'
 
-export async function listDatabases (db: PoolClient): Promise<string[]> {
-  const listDbQuery = 'SELECT datname FROM pg_database'
-  const dbResults = await db.query(listDbQuery)
-  return dbResults.rows.map(r => r.datname)
+export type Database = {
+  id: number;
+  name: string;
+  collation: string;
+  encoding: string;
+  ctype: string;
 }
+
+export async function listDatabases (db: PoolClient): Promise<Database[]> {
+  const listDbQuery = 'SELECT  oid AS id, ' +
+                              'datname AS name, ' +
+                              'pg_encoding_to_char(encoding) AS encoding, ' +
+                              'datcollate AS collation, ' +
+                              'datctype AS ctype ' +
+                              'FROM pg_database;'
+
+  const dbResults: QueryResult<Database> =
+    await db.query<Database>(listDbQuery)
+
+  return dbResults.rows.filter(r => (
+    r.name !== 'template0' &&
+      r.name !== 'template1'
+  ))
+}
+
+export type Schema = {
+  name: string;
+  owner: string;
+}
+
+export async function listSchemas (db: PoolClient): Promise<Schema[]> {
+  const listSchemaQuery = 'SELECT ' +
+                            'schema_name AS name, ' +
+                            'schema_owner AS owner ' +
+                          'FROM information_schema.schemata'
+
+  const dbResults: QueryResult<Schema> =
+    await db.query<Schema>(listSchemaQuery)
+
+  return dbResults.rows.filter(r => (
+    !r.name.startsWith('pg_')
+  ))
+}
+
